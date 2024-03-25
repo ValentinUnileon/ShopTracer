b7c5c7e3763f0311d76e0f9e59c2b4d4e9aff6fc
ValentinUnileon <74121671+ValentinUnileon@users.noreply.github.com>
2024-03-25 15:24:29 +0100

    Clock implemented

--- View/principalView.py
+++ View/principalView.py
@@ -1,10 +1,13 @@
 import customtkinter as ctk
 from tkinter import PhotoImage
 from PIL import Image
+import time
 
 class PrincipalView:
 
     buttonIcon = None  
+    label_hora = None
+    app= None
      #CONSTRUCTOR de la clase - se le pasa como argumento el controlador
 
     def __init__(self, viewController): 
@@ -18,13 +21,18 @@ class PrincipalView:
     def controllerData(self):
         return self.viewController.getNum()
     
+    def update_hour(self):
+        hora_actual = time.strftime('%H:%M:%S')
+        self.label_hora.configure(text=hora_actual)
+        self.app.after(1000, self.update_hour)
+    
     def runAppInterface(self):  # Agregar self como primer argumento
 
         #Creacion de la pantalla principal
         ctk.set_appearance_mode("System")           # Modes: system (default), light, dark
         ctk.set_default_color_theme("blue")         # Themes: blue (default), dark-blue, green
-        app = ctk.CTk()                             # create CTk window like you do with the Tk window
-        app.geometry("1420x650")
+        self.app = ctk.CTk()                             # create CTk window like you do with the Tk window
+        self.app.geometry("1420x650")
 
 
         def button_function():
@@ -33,19 +41,43 @@ class PrincipalView:
 
         # Crear el primer frame (columna izquierda)
             
-        left_column = ctk.CTkFrame(app, width=350)
+        left_column = ctk.CTkFrame(self.app, width=350)
         left_column.pack(side="left", fill="y", padx=0, pady=0)     # Columna izquierda ocupa toda la altura
+
+
+            #Boton dentro del frame
+
+        button = ctk.CTkButton(master=left_column, text="CTkButton", command=button_function)
+        button.place(relx=0.1, rely=0.5, anchor=ctk.W)  # Coloca el botón a la izquierda
+
+
+            #Campo de texto dentro del frame
+
+        textbox = ctk.CTkTextbox(master=left_column, width=200, corner_radius=0, fg_color="transparent")
+        textbox.grid(row=0, column=0, sticky="nsew")
+        textbox.insert("0.0", "Some example text!\n")
+
+        self.label_hora = ctk.CTkLabel(left_column, font=('Roboto', 48), fg_color='transparent')
+        self.label_hora.grid(row=0, column=0, padx=10, pady=10)
+        self.label_hora.configure(text="Nuevo texto")
+
+        self.update_hour()
+
+
+
+
+
 
         #Frame de categorias a la derecha
 
 
-        categoriesFrame = ctk.CTkFrame(app, width=370 ,fg_color="green")
+        categoriesFrame = ctk.CTkFrame(self.app, width=370 ,fg_color="green")
         categoriesFrame.pack(side="right", fill="y", padx=0, pady=0)
 
         # Crear el segundo frame (frame para mostrar la imagen del plano)
 
 
-        planeFrame = ctk.CTkFrame(app, height=400, width=900, fg_color="transparent")
+        planeFrame = ctk.CTkFrame(self.app, height=400, width=900, fg_color="transparent")
         planeFrame.pack(anchor='nw', padx=0, pady=0)  # Relativo al ancho y alto de la ventana nw = north west padx y pady separaciones
 
             #se establece la imagen del plano
@@ -59,7 +91,7 @@ class PrincipalView:
 
         #Tercer frame debajo del plano
 
-        shopFrame = ctk.CTkFrame(app, height=250, width=900, fg_color="transparent")
+        shopFrame = ctk.CTkFrame(self.app, height=250, width=900, fg_color="transparent")
         shopFrame.pack(anchor = 'sw', padx=0, pady=0)
 
 
@@ -85,18 +117,11 @@ class PrincipalView:
         # El valor de relwidth indica el ancho relativo al ancho total de la ventana.
         # El valor de relheight indica la altura relativa al alto total de la ventana.
 
-        #Boton dentro del frame
 
-        button = ctk.CTkButton(master=left_column, text="CTkButton", command=button_function)
-        button.place(relx=0.1, rely=0.5, anchor=ctk.W)  # Coloca el botón a la izquierda
 
-        #Campo de texto dentro del frame
 
-        textbox = ctk.CTkTextbox(master=left_column, width=200, corner_radius=0, fg_color="transparent")
-        textbox.grid(row=0, column=0, sticky="nsew")
-        textbox.insert("0.0", "Some example text!\n")
 
-        app.mainloop()
+        self.app.mainloop()
 
       
 
